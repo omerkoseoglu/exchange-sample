2027c4819196291b0a68ddfa4478bebc0f47c1e2
omer.koseoglu <omer.koseoglu@derinarge.com>
2020-02-05 09:39:08 +0300

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exchange-sample';
+
+  constructor(
+    private http: HttpClient
+  ) {
+
+    const headers = new HttpHeaders({ 'Content-Type': 'text/xml' }).set('Accept', 'text/xml');
+
+    this.http.get(`/kurlar/202002/03022020.xml`, { headers, responseType: 'text' })
+      .subscribe(result => {
+        console.log(result);
+      });
+  }
 }
